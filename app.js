7ef56ad05f107d4fb96e6538ba1cc4f3170e348b
Ernesto Rubio Fuentes <neto.r27@gmail.com>
2017-10-12 19:44:51 +0000

    Adding first 2 get routes

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log("Hello world!");
+var express = require('express');
+
+var app = express();
+
+// If running in cloud9
+var port = process.env.PORT;
+var host = process.env.IP;
+
+// If running in localhost
+// var port = 5000;
+// var host = 'localhost';
+
+
+app.get('/', function(req, res){
+    res.send('Hello World!');
+});
+
+app.get('/books', function(req, res){
+    res.send('Hello Books!');
+});
+
+app.listen(port, host, function(err){
+    if(err){
+        console.log(err)
+    }else{
+        console.log('running server on port ' + port);
+    }
+});
