dd99767d27204a5fce02a63b6f99e4b6c178d66c
Ernesto Rubio Fuentes <neto.r27@gmail.com>
2017-10-16 16:38:13 +0000

    Adding ejs template

--- app.js
+++ app.js
@@ -3,22 +3,23 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 5000;
-var host = process.env.IP || 'localhost';
+//var host = process.env.IP || 'localhost';
 
 // set the static directory
 app.use(express.static('public'));
-app.use(express.static('src/views'));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 // set the routes
 app.get('/', function(req, res){
-    res.send('Hello World!');
+    res.render('index', { title: 'Hello from render', list: ['a', 'b'] });
 });
 
 app.get('/books', function(req, res){
     res.send('Hello Books!');
 });
 
-app.listen(port, host, function(err){
+app.listen(port, function(err){
     if(err){
         console.log(err);
     }else{
